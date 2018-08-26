e6ba0a60b9ec87bd8d7a9cdc35f381b14a3bb4fe
Oksana <oksana.barinova12@outlook.com>
2018-08-26 15:21:58 +0300

    For home task 1: 2 methods for reflection; one of them is with recursion. For home task 2: Method contained reflection and file saving.

---
+++ Reflection/Reflection/IterateProperties.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class IterateProperties
+    {
+
+        public void IterateThrougObjectProperties(object p)
+        {
+            foreach (var prop in p.GetType().GetProperties())
+            {
+                if (!prop.PropertyType.IsValueType && prop.PropertyType != typeof(string))
+                    IterateThrougObjectProperties(prop.GetValue(p));
+
+                Console.WriteLine($"PropertyName: {prop.Name}, PropertyType : {prop.PropertyType.Name}, PropertyValue : {prop.GetValue(p)}");
+            }
+
+
+
+        }
+
+        public void IterateThrougObjectPropertiesPrintSave<T>(T obj)
+        {
+            string s = string.Empty;
+            
+            foreach (var prop in obj.GetType().GetProperties())
+            {
+                if (prop.PropertyType.IsValueType || prop.PropertyType == typeof(string))
+                    s+=$"*{prop.Name}* = *{prop.GetValue(obj)}*\n";
+            }
+            //s += $"Time {DateTime.Now}";
+            File.WriteAllText("C:\\Temp\\file.txt", s);
+            Console.WriteLine(s);
+
+        }
+
+    }
+    public class BankCardInfo
+    {
+        //[SecureData]
+        public string Cvv { get; set; }
+        //[SecureData]
+        public string Pin { get; set; }
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+    }
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string Email { get; set; }
+
+        public BankCardInfo CardInfo { get; set; }
+
+        //[SecureData]
+        public string Passowrd { get; set; }
+    }
+
+
+
+
+}
+
+
+
---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IterateProperties ip = new IterateProperties();
+            Person p = new Person()
+            {
+                Age = 21,
+                CardInfo = new BankCardInfo(),
+                Email = "some@some.com",
+                FirstName = "Tim",
+                LastName = "R",
+                Passowrd = "password1"
+            };
+            ip.IterateThrougObjectProperties(p);
+            ip.IterateThrougObjectPropertiesPrintSave(p);
+            Console.ReadLine();
+
+        }
+    }
+}
